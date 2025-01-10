564db6af7b01aa3f4be47217c5d5542a7fa6cd4e
unknown <iwanhprasetyo14@gmail.com>
2025-01-10 11:53:10 +0700

    'Update-ReactNative0.76'

---
+++ index.js
@@ -0,0 +1,65 @@
+
+import { NativeModules, Platform } from 'react-native'
+
+var { RNImageConverter } = NativeModules;
+
+class IImageConverter {
+    static convert(param) {
+        if (param.hasOwnProperty('grayscale')) {
+            param.grayscale = param.grayscale.toString().toLowerCase();
+        } else {
+            param.grayscale = "false";
+        }
+        if (param.hasOwnProperty('base64')) {
+            param.base64 = param.base64.toString().toLowerCase();
+        } else {
+            param.base64 = "false";
+        }
+        if (param.hasOwnProperty('resizeRatio')) {
+            param.resizeRatio = IImageConverter.checkToInputValue(param.resizeRatio);
+        } else {
+            param.resizeRatio = "1.0";
+        }
+        if (param.hasOwnProperty('imageQuality')) {
+            param.imageQuality = IImageConverter.checkToInputValue(param.imageQuality);
+        } else {
+            param.imageQuality = "1.0";
+        }
+
+        try {
+            if (Platform.OS === "ios") {
+                return RNImageConverter.imageConvert(param).then(result => (result));
+            } else if (Platform.OS === "android") {
+                return new Promise((resolve) => {
+                    RNImageConverter.imageConvert(param, resolve);
+                  });
+            } else {
+                return {
+                    success: false,
+                    errorMsg: "not yet supported.("+Platform.OS+")"
+                }
+            }
+        } catch(error) {
+            return {
+                success: false,
+                errorMsg: "check your native("+Platform.OS+") module setting."
+            }
+        }
+    }
+
+    static checkToInputValue(inputValue) {
+        try {
+            var checkDataToString = inputValue.toString();
+            var checkData = parseFloat(checkDataToString);
+            if (checkData <= 0 || checkData > 1.0) {
+                return "1.0";
+            } else {
+                return checkDataToString;
+            }
+        } catch(error) {
+            return "1.0";
+        }
+    }
+}
+
+export default IImageConverter;
---
+++ ios/RNImageConverter.h
@@ -0,0 +1,20 @@
+//
+//  RNImageConverter.h
+//
+//  Created by Kellin on 2019/10/20.
+//  Copyright © 2019 https://github.com/LeeKyoungIl/react-native-image-converter. All rights reserved.
+//
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface RNImageConverter : NSObject <RCTBridgeModule>
+
+@end
+  
